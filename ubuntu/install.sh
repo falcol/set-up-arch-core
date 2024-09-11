854d48d058be2239da9a4f1d4787c7256c916c49
Đình Ngọc <43593736+falcol@users.noreply.github.com>
2024-09-11 20:12:47 +0700

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -16,7 +16,7 @@ env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['Bamb
 sudo apt install telegram-desktop
 
 ## NODEJS
-curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs -y
 
